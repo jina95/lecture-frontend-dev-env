e27015154413234ce8cfc0b0177485e5288dfa21
jina95 <spzn02@gmail.com>
2020-10-03 15:10:29 +0900

    webpack-1/loader/실습완료

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,25 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'url-loader',
+        options: {
+          publicPath: './dist/',
+          name: '[name].[ext]?[hash]',
+          limit: 20000
+        }
+      }
+    ]
   }
 }
