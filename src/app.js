bb112f2f45dfa1ba4c908d01ea9ccc56c258e0e5
jina95 <spzn02@gmail.com>
2020-10-24 23:12:44 +0900

    webpack-4/devServer/실습완료

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import MainController from "./controllers/MainController.js";
 import "./app.scss";
+// import axios from "axios";
 
 document.addEventListener("DOMContentLoaded", () => {
   new MainController();
 });
+// document.addEventListener("DOMContentLoaded", async () => {
+//   new MainController();
+//   const { data } = await axios.get("/api/keywords");
+//   return data;
+// });
